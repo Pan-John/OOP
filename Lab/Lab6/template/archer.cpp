1e778bd7c66c17f488398e6688bef8ae5ffdaf45
eth-Pan <ericpan9889@gmail.com>
2023-02-20 22:31:20 +0800

    xxx

--- Lab/Lab6/template/archer.cpp
+++
@@ -1,7 +0,0 @@
-#include "archer.h"
-Archer::Archer(string roleID, int roleLevel, int roleMoney, int roleHp) : Role(roleID, roleLevel, roleMoney, roleHp) {
-
-}
-void Archer::ShowRole() {
-	cout << "Role " << getID() << " is Archer" << endl;
-}
